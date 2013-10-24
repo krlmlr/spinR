33276d7b0feba68fcd91e952d9da13fb38e5f595
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-10-24 14:52:00 +0200

    don't depend on the name of the spin directory

--- spin.R
+++ spin.R
@@ -3,5 +3,6 @@ options(knitr.width=60)
 render_markdown()
 inline.hook.xspace <- function(...) { paste0(knitr:::.inline.hook.tex(...), '\\xspace') }
 knit_hooks$set(inline=inline.hook.xspace)
-setwd("spin")
-spin(file.path("..", commandArgs(TRUE)), report=F)
+stopifnot(length(commandArgs(TRUE)) == 2)
+setwd(commandArgs(TRUE)[2])
+spin(file.path("..", commandArgs(TRUE)[1]), report=F)
