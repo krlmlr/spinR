77542583d83a499760c6717a1fcafa3148eea74a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-10-24 14:42:18 +0200

    #1: attempts to enable concordance don't show any new file

--- spin.R
+++ spin.R
@@ -3,5 +3,6 @@ options(knitr.width=60)
 render_markdown()
 inline.hook.xspace <- function(...) { paste0(knitr:::.inline.hook.tex(...), '\\xspace') }
 knit_hooks$set(inline=inline.hook.xspace)
+opts_knit$set(concordance=TRUE)
 setwd("spin")
-spin(file.path("..", commandArgs(TRUE)), report=F)
+spin(file.path("..", commandArgs(TRUE)), report=F, precious=T)
