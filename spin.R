cc9503c26a4040716f93677c5a43fe609a2d107d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-11-21 11:42:52 +0100

    adapt to new option syntax in knitr

--- spin.R
+++ spin.R
@@ -1,5 +1,5 @@
 library(knitr)
-options(knitr.width=60)
+options(knitr.package.width=60)
 render_markdown()
 inline.hook.xspace <- function(...) { paste0(knitr:::.inline.hook.tex(...), '\\xspace') }
 knit_hooks$set(inline=inline.hook.xspace)
